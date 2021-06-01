18f7ec1e0620868613627b60b0b84497d43e9aa4
BilguunTs <usosketch@yahoo.com>
2021-06-01 19:20:52 +0800

    adding generics

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -166,7 +166,9 @@ bool SimpleEQAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleEQAudioProcessor::createEditor()
 {
-    return new SimpleEQAudioProcessorEditor (*this);
+   // return new SimpleEQAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
+    
 }
 
 //==============================================================================
